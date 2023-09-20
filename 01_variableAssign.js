0e64c0082a879a809a9a7ef340398ec30959cde1
patilsantosha <santosh.patil4@gmail.com>
2023-09-20 17:35:57 +0530

    JS basic with variable
    JS Basics with variable

---
+++ 01_variableAssign.js
@@ -0,0 +1,22 @@
+var myFullName;
+console.log("Variable without initialization:",myFullName);
+myFullName="Santoshkumar Patil"
+console.log("My Full Name is:",myFullName);
+myFullName="Santoshkumar Ganpatrao Patil"
+console.log("My updated Name is:",myFullName);
+var myLove="I love only JS";
+console.log(myLove);
+var firstName="Santoshkumar";
+var lastName="Patil";
+var age="35";
+var address="Kolhapur";
+var pinCode="416112";
+var gender="Male";
+var isMarried="Married";
+console.log("First Name:",firstName);
+console.log("Last Name:",lastName);
+console.log("Age:",age);
+console.log("Address:",address);
+console.log("Pincode:",pinCode);
+console.log("Gender:",gender);
+console.log("Is Married:",isMarried);

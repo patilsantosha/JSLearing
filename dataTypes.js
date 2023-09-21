c2741836eeacf5b7d9fb1fda97727dde9499c55f
patilsantosha <santosh.patil4@gmail.com>
2023-09-21 08:52:48 +0530

    JS data types
    JS basic data types

---
+++ dataTypes.js
@@ -0,0 +1,28 @@
+
+
+
+var age=40;
+console.log(typeof age);
+var price=34.958;
+console.log(price, typeof price);
+var firstName="Santoshkumar";
+console.log(firstName, typeof firstName);
+var city="Pune";
+console.log("Name of City: ", city, "Data type:", typeof city);
+var hscMarks= '78';
+console.log(hscMarks, typeof hscMarks);
+
+var isMarried = true;
+console.log(isMarried, typeof isMarried);
+
+var isHandicap = false;
+console.log(isHandicap, typeof(isHandicap));
+
+console.log(typeof "I love only JS");
+console.log(typeof 204567);
+console.log(typeof false);
+console.log('--------null vs undefined-------');
+var accountNumber;
+console.log(accountNumber);
+accountNumber = null
+console.log(accountNumber);

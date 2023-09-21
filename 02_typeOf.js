8c39c0526269553f200a204246ddce57631081f0
patilsantosha <santosh.patil4@gmail.com>
2023-09-21 12:24:55 +0530

    assignment on basic data tpes of JS
    assignment on basic data tpes of JS

---
+++ 02_typeOf.js
@@ -0,0 +1,21 @@
+
+
+console.log("=========================First Step=================================","\n");
+var bankName;
+console.log("Before initialization:",bankName, "(Type Data type):",typeof bankName);
+bankName='SBI Bank';
+console.log("After initialization:",bankName,"(Type of Data):",typeof bankName);
+console.log("=========================Second Step=================================","\n");
+var marks="90%";
+console.log("Total %:",marks,"(Type of Data):",typeof marks);
+console.log("=========================Third Step=================================","\n");
+var isWorking;
+console.log("Before initialization:",isWorking,"(type of data):", typeof isWorking);
+isWorking=true;
+console.log("After initialization:",isWorking,"(Type of data):",typeof isWorking);
+console.log("=========================Third Step=================================","\n");
+var totalCount="Hundred and Seven";
+console.log("Total count is:",totalCount,"(type of data):", typeof totalCount);
+totalCount=107;
+console.log("Updated count is:",totalCount,"(Type of data):", typeof totalCount);
+console.log("=========================Finished all Step=================================");
---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,18 @@
+
+
+var mobileNo=706252235;
+var firstName="Santoshkumar";
+var isPass=true;
+console.log("*********************Basic Data types of JS Number, String & Boolean*****************","\n");
+console.log("Mobile no:",mobileNo, "type of Data:" ,typeof mobileNo);
+console.log("First Name:", firstName,"Type of Data:", typeof firstName);
+console.log("Is pass or Fail:", isPass,"Type of Data:", typeof isPass,"\n");
+console.log("***********************Difference between Null and Undefined*************************","\n");
+
+console.log("When we declare variable before initialization in variable one value will be there & that is undefined");
+var age;
+console.log("Before initialization value:", typeof age);
+age=null;
+console.log("After initialization NUll:",age,"\n");
+console.log("*******************Null means empty nothing will be there in variable 0 also*********");
+

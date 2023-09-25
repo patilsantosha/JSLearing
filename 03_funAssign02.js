552dad286659f6855b8dc31cafd2f217c53e8ade
patilsantosha <santosh.patil4@gmail.com>
2023-09-26 04:09:47 +0530

    Basic string functions
    Basic string functions

---
+++ 03_funAssign02.js
@@ -0,0 +1,13 @@
+
+
+function bankDetails(bankName, accountNumber, location, pinCode)
+{
+    
+    console.log("Bank Name:=>", bankName, "Account Number:=>", accountNumber,"Location:=>", location, "Pincode:=>", pinCode);
+    console.log("Bank Name:=>", bankName, "Account Number:=>", accountNumber,"Location:=>", location, "Pincode:=>", pinCode);
+    console.log("Bank Name:=>", bankName, "Account Number:=>", accountNumber,"Location:=>", location, "Pincode:=>", pinCode);
+}
+console.log("==========================Bank Details================================");
+bankDetails("CITI Bank", 3456782345, 'PUNE', 431202);
+bankDetails("Axis Bank", 7856782345, 'Mumbai', 441202);
+bankDetails("HDFC Bank", 8956782345, 'PUNE', 631202, "OPEN");
---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,23 @@
+
+
+ function stringBasics(){
+    console.log("========================step 1==========================");
+    console.log("My dream company Name is : Infosys");
+    console.log("========================step 2==========================");
+   var myHobby1="Traveling";
+   var myHobby2="Playing";
+    var myHobby3="Swiming";
+
+    console.log("My Hobbies are: Traveling, Playing, Swiming");
+
+   var str1=myHobby1.length;
+    console.log("My Hobby one Total Characters are:", str1); 
+
+    var str2=myHobby2.length;
+    console.log("My Hobby Two Total Characters are:", str2);
+
+    var str3=myHobby3.length;
+   console.log("My Hobby three Total Characters are:", str3);
+}
+    
+stringBasics();

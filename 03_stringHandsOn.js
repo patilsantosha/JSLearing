9fce5b6aa60329bd54036e105be9b922ef6dedb2
patilsantosha <santosh.patil4@gmail.com>
2023-09-26 19:39:53 +0530

    string functions
    string functions

---
+++ 03_stringHandsOn.js
@@ -0,0 +1,40 @@
+
+
+console.log("====================Assignment 02===================");
+var strmsg="   Hey you are doing good, keep it up    ";
+
+function stringHandsOn(){
+console.log("Given string is:==>", strmsg);
+console.log("Length of string==>", strmsg.length);
+console.log('=================================================');
+
+var strtrim=strmsg.trim();
+console.log("String after trim==>", strtrim);
+
+console.log('Length of string after trim==>', strtrim.length);
+var result=strmsg.length-strtrim.length;
+console.log("Total Number of spaces removed ==>", result);
+console.log('=================================================');
+console.log("First character after trim:==>", strtrim.charAt(0),"Last character after trim:==>",strtrim.charAt(33));
+console.log('=================================================');
+var result=strtrim.split( );
+console.log("string after split ==>", result);
+
+var words=strtrim.split(" ");
+console.log("Total word available in string==>", words.length);
+console.log('=================================================');
+console.log(`Index of "good is ==>"`, strtrim.indexOf('g'));
+console.log(`substring starting from Index "22" is ==>`, strtrim.substring(22));
+console.log('=================================================');
+console.log(`String ends with "up"==>`, strtrim.endsWith('up'));
+console.log('=================================================');
+console.log(`String start with 'Hey' ==>`, strtrim.startsWith('Hey'));
+
+
+
+
+
+
+
+}
+stringHandsOn();
---
+++ 03_stringTemplateAssig.js
@@ -0,0 +1,13 @@
+
+
+console.log("==================Step 1==================");
+console.log("My dream company is: Infosys");
+var hobby1="Playing";
+var hobby2="Swiming";
+var hobby3="reading";
+
+console.log("==================Step 2==================");
+console.log("my hobbies are:=>", hobby1, hobby2, hobby3);
+console.log("==================Step 3==================");
+var strconcat=hobby1+hobby2+hobby3;
+console.log("After Concatenating Hobbies:=>",strconcat);

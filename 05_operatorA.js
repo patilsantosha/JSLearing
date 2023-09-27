44e6cbd1f155bb9214a7c69edf15e297225aaec8
patilsantosha <santosh.patil4@gmail.com>
2023-09-27 19:32:24 +0530

    Javascript operators
    Javascript operators

---
+++ 05_operatorA.js
@@ -0,0 +1,69 @@
+
+
+console.log("==========================Assignment-01============================");
+function squareOfwordLength(str1){
+    console.log("Given word is ==>", str1);
+    var result=str1.length;
+    console.log(`Given string is:==> ${str1.length}`);
+    var sqr1=result*result;
+
+    return sqr1;
+}
+var result=squareOfwordLength("JavaScript");
+    console.log(`Length square of given word is==> ${result}`);
+console.log("================================================================");
+    function squareOfwordLength(str1){
+        console.log("Given word is ==>", str1);
+        var result=str1.length;
+        console.log(`Given string is:==> ${str1.length}`);
+        var sqr1=result*result;
+    
+        return sqr1;
+    }
+    var result=squareOfwordLength("Google Chrome");
+        console.log(`Length square of given word is==> ${result}`);
+
+        console.log("================================================================");
+        function squareOfwordLength(str1){
+        console.log("Given word is ==>", str1);
+        var result=str1.length;
+        console.log(`Given string is:==> ${str1.length}`);
+        var sqr1=result*result;
+    
+        return sqr1;
+    }
+    var result=squareOfwordLength("Developer Smart");
+        console.log(`Length square of given word is==> ${result}`);
+
+        console.log("================================================================");
+function andeveloper(){
+    var givenstring="I am Angular Developer";
+    console.log(`Given string is ==> ${givenstring}`);
+
+    console.log(`Length of given string is ==> ${givenstring.length}`);
+
+    var words=givenstring.split(" ");
+console.log(`Words In given string are ==> ${words}`);
+console.log(`Words In given string are ==> ${words.length}`);
+   var  words=givenstring.length/4;
+console.log("=================================================================");
+   console.log(`After Division Total number of word in given string ==> ${words}`);
+
+   var str=givenstring.length*4;
+   console.log("=================================================================");
+   console.log(`After Multiplication Total words available in string are ==> ${str}`);
+}
+
+andeveloper();
+
+
+
+
+
+
+
+
+ 
+
+
+    
---
+++ 05_operatorB.js
@@ -0,0 +1,88 @@
+
+console.log(`==============================Assignment 02=================================`);
+function greaterNumber(num1, num2){
+    console.log(`Given values are ==> ${num1} ${num2}`);
+    var no=num1>=num2 ? " 10 is Greater" : " -10 is Greater";
+    console.log(`${no}`);
+}
+
+greaterNumber(10, -10);
+console.log(`=============================================`);
+
+function greaterno(num1, num2){
+
+    console.log(`Given values are ==> ${num1} ${num2}`);
+    var no=num1>=num2 ? " 800 is Greater" : " 899 is Greater";
+    console.log(`${no}`);
+}
+greaterno(800, 899);
+console.log(`=============================================`);
+
+function isEvenOrOddNum(num){
+    console.log(`We have given number is == >${num}`);
+var no=num%2==0? "number is Even" : " number is odd";
+console.log(no);
+}
+isEvenOrOddNum(29);
+
+console.log(`=============================================`);
+
+function isEvenOrOddNum(num){
+    console.log(`We have given number is == > ${num}`);
+var no=num%2==0 ? "number is Even" : " number is odd";
+console.log(no);
+}
+isEvenOrOddNum(44);
+
+console.log(`=============================================`);
+
+function isEvenOrOddNum(num){
+    console.log(`We have given number is == > ${num}`);
+var no=num%2==0 ? "number is Even" : " number is odd";
+console.log(no);
+}
+isEvenOrOddNum(0);
+
+console.log(`=============================================`);
+
+function isEvenOrOddNum(num){
+    console.log(`We have given number is == >${num}`);
+var no=num%2==0 ? "number is Even" : " number is odd";
+console.log(no);
+}
+isEvenOrOddNum(101);
+
+console.log(`=============================================`);
+
+function wordLength(str){
+    console.log(`We have given string is == > ${str}`);
+    console.log(`Total characters in string are==>${str.length}`);
+var no=str.length%2==0 ? "string is Even" : "string is odd";
+console.log(no);
+}
+wordLength("JavaScript");
+
+console.log(`=============================================`);
+
+function wordLength(str){
+    console.log(`We have given string is ==> ${str}`);
+    console.log(`Total characters in string are==>${str.length}`);
+var no=str.length%2==0 ? "string is Even" : "string is odd";
+console.log(no);
+}
+wordLength("Developer");
+
+console.log(`=============================================`);
+
+function wordLength(str){
+    console.log(`We have given string is== > ${str}`);
+    console.log(`Total characters in string are==>${str.length}`);
+var no=str.length%2==0 ? " string is Even" : " string is Odd";
+console.log(no);
+}
+wordLength("Google");
+
+
+
+
+
